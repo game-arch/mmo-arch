d174b8cd78e5c9c2e354b13aac023f8cee1048f7
jhenry123 <jhenry@cytracom.com>
2019-11-22 00:02:06 -0600

    removed unused code

--- game-servers/src/server/lobby/lobby.gateway.ts
+++ game-servers/src/server/lobby/lobby.gateway.ts
@@ -3,24 +3,19 @@ import {
     OnGatewayInit,
     WebSocketGateway,
     WebSocketServer
-}                                 from "@nestjs/websockets";
-import {Server, Socket}           from "socket.io";
-import {Events}                   from "../../../lib/constants/events";
-import {LobbyService}             from "./lobby.service";
-import {GameWorld}                from "../../../lib/entities/game-world";
-import {PresenceClient}           from "../../microservice/presence/client/presence.client";
-import {interval, merge, Subject} from "rxjs";
-import {EventPattern}             from "@nestjs/microservices";
+}                       from "@nestjs/websockets";
+import {Server, Socket} from "socket.io";
+import {Events}         from "../../../lib/constants/events";
+import {LobbyService}   from "./lobby.service";
+import {GameWorld}      from "../../../lib/entities/game-world";
+import {PresenceClient} from "../../microservice/presence/client/presence.client";
 
 @WebSocketGateway()
-export class LobbyGateway implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect {
+export class LobbyGateway implements OnGatewayConnection, OnGatewayInit {
     @WebSocketServer()
     server: Server;
     socket: Socket;
-
     servers: GameWorld[] = [];
-
-    pull = new Subject();
 
     constructor(private service: LobbyService, private presence: PresenceClient) {
 
@@ -39,16 +34,5 @@ export class LobbyGateway implements OnGatewayConnection, OnGatewayInit, OnGatew
 
     async afterInit(server: Server) {
         this.servers = await this.presence.getServers();
-    }
-
-    handleDisconnect(client: Socket) {
-        this.pull.next();
-    }
-
-    @EventPattern(Events.SERVER_LIST)
-    serverList(servers: GameWorld[]) {
-        console.log('got event!');
-        this.servers = servers;
-        this.server.emit(Events.SERVER_LIST, servers);
     }
 }
