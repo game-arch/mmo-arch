93f9f11695420cf6b3cbb611d8ca0fea21f26f42
jhenry123 <jhenry@cytracom.com>
2019-12-03 10:23:07 -0600

    added chat action placeholders. WIP

--- server/services/chat/chat.controller.ts
+++ server/services/chat/chat.controller.ts
@@ -1,9 +1,58 @@
 import {Controller, Get} from '@nestjs/common';
-import {ChatService}     from './chat.service';
+import {ChatService} from './chat.service';
+import {
+    EventPattern,
+    MessagePattern
+} from "@nestjs/microservices";
+import {
+    CharacterOffline,
+    CharacterOnline
+} from "../character/actions";
+import {GlobalMessage, LocalMessage, PrivateMessage, RegionMessage, TradeMessage, ZoneMessage} from "./actions";
 
 @Controller()
 export class ChatController {
     constructor(private readonly service: ChatService) {
+    }
+
+    @MessagePattern(CharacterOnline.event)
+    characterOnline(data: CharacterOnline) {
+
+    }
+
+    @MessagePattern(CharacterOffline.event)
+    characterOffline(data: CharacterOffline) {
+
+    }
+
+    @MessagePattern(LocalMessage.event)
+    localMessage(data: LocalMessage) {
+
+    }
+
+    @MessagePattern(RegionMessage.event)
+    regionMessage(data: RegionMessage) {
+
+    }
+
+    @MessagePattern(ZoneMessage.event)
+    zoneMessage(data: ZoneMessage) {
+
+    }
+
+    @MessagePattern(TradeMessage.event)
+    tradeMessage(data: TradeMessage) {
+
+    }
+
+    @MessagePattern(GlobalMessage.event)
+    globalMessage(data: GlobalMessage) {
+
+    }
+
+    @MessagePattern(PrivateMessage.event)
+    privateMessage(data: PrivateMessage) {
+
     }
 
 }
