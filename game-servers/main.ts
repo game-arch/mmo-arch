5fe406ea3251209867e9b9d6fc0da4de3103b103
jhenry123 <jhenry@cytracom.com>
2019-12-03 09:08:56 -0600

    removed main.ts

--- game-servers/main.ts
+++
@@ -1,7 +0,0 @@
-import * as path from "path";
-
-require('dotenv').config({
-    path: path.resolve(__dirname, '../.env')
-});
-
-require('./services/' + (process.env.SERVER_TYPE || 'lobby') + '/main');
