2291692f20c4a01dd2de521f3a29c4d68162e505
Justin Henry <jwhenry3@yahoo.com>
2020-03-09 16:46:06 -0500

    adjusted icon size compared to action button

--- client/projects/game/src/lib/svg/icon.component.ts
+++ client/projects/game/src/lib/svg/icon.component.ts
@@ -3,9 +3,17 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: "icon",
     template: `
-        <svg viewBox="0 0 512 512" [style.width.px]="size" [style.height.px]="size">
-            <use [attr.xlink:href]="'#icon-' + name"/>
-        </svg>
+        <ng-container *ngIf="size">
+            <svg viewBox="0 0 512 512" [style.width.px]="size" [style.height.px]="size">
+                <use [attr.xlink:href]="'#icon-' + name"/>
+            </svg>
+        </ng-container>
+
+        <ng-container *ngIf="!size">
+            <svg viewBox="0 0 512 512" [style.width]="'100%'" [style.height]="'100%'">
+                <use [attr.xlink:href]="'#icon-' + name"/>
+            </svg>
+        </ng-container>
     `
 })
 export class IconComponent {
@@ -13,5 +21,5 @@ export class IconComponent {
     @Input()
     name = "";
     @Input()
-    size = 32;
+    size = null;
 }
--- client/projects/game/src/lib/svg/svg.module.ts
+++ client/projects/game/src/lib/svg/svg.module.ts
@@ -1,9 +1,10 @@
 import { NgModule }       from "@angular/core";
 import { IconComponent }  from "./icon.component";
 import { IconsComponent } from "./icons.component";
+import { CommonModule }   from "@angular/common";
 
 @NgModule({
-    imports     : [],
+    imports     : [CommonModule],
     declarations: [IconComponent, IconsComponent],
     exports     : [IconComponent, IconsComponent]
 })
