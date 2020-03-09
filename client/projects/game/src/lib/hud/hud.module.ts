9be3d9085a746b9b26cdd7b6a500f6526c1c32b3
Justin Henry <jwhenry3@yahoo.com>
2020-03-08 21:43:27 -0500

    added hover and active states for the action buttons

--- client/projects/game/src/lib/hud/hud.module.ts
+++ client/projects/game/src/lib/hud/hud.module.ts
@@ -1,15 +1,16 @@
 import { AvatarComponent } from './avatar/avatar.component'
 import { JoystickComponent } from './controls/joystick/joystick.component'
 import { ActionButtonComponent } from './controls/button/action-button.component'
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { ConnectionModule } from '../connection/connection.module'
-import { HudComponent } from './hud.component'
+import { NgModule }           from '@angular/core'
+import { CommonModule }       from '@angular/common'
+import { ConnectionModule }   from '../connection/connection.module'
+import { HudComponent }       from './hud.component'
 import { ActionBarComponent } from './action-bar/action-bar.component'
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { NgxJoystickModule } from 'ngx-joystick'
-import { SceneFactory } from '../game-engine/phaser/scenes/scene-factory.service'
-import { BarComponent } from './progress/bar.component'
+import { FlexLayoutModule }   from '@angular/flex-layout'
+import { NgxJoystickModule }  from 'ngx-joystick'
+import { SceneFactory }       from '../game-engine/phaser/scenes/scene-factory.service'
+import { BarComponent }       from './progress/bar.component'
+import { MatRippleModule }    from "@angular/material/core";
 
 @NgModule({
     declarations: [
@@ -21,6 +22,7 @@ import { BarComponent } from './progress/bar.component'
         AvatarComponent,
     ],
     imports: [
+        MatRippleModule,
         CommonModule,
         ConnectionModule,
         FlexLayoutModule,
