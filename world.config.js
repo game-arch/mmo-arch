713192777f1a3a9c37c9501b1d02b2a1b4ba9a40
Justin Henry <jwhenry3@yahoo.com>
2020-03-15 11:35:58 -0500

    added glob to the world.config so we dont have to add entries to the file any more when adding a new microservice

--- world.config.js
+++ world.config.js
@@ -1,36 +1,12 @@
+let glob = require('glob')
+let directories = glob.sync('dist/nest/local/*')
 module.exports = {
-    apps: [
-        {
-            instances: 1,
-            name: (process.env.WORLD_CONSTANT || 'maiden') + '-character',
-            script: 'dist/nest/local/character/main.js',
-            watch: ['dist/nest/local/character', 'dist/nest/lib'],
-            env: {
-                WORLD_CONSTANT: process.env.WORLD_CONSTANT,
-                WORLD_NAME: process.env.WORLD_NAME
-            },
-            watch_options: {
-                'followSymlinks': false
-            }
-        },
-        {
-            instances: 1,
-            name: (process.env.WORLD_CONSTANT || 'maiden') + '-party',
-            script: 'dist/nest/local/party/main.js',
-            watch: ['dist/nest/local/party', 'dist/nest/lib'],
-            env: {
-                WORLD_CONSTANT: process.env.WORLD_CONSTANT,
-                WORLD_NAME: process.env.WORLD_NAME
-            },
-            watch_options: {
-                'followSymlinks': false
-            }
-        },
-        {
-            instances: 4,
-            name: (process.env.WORLD_CONSTANT || 'maiden') + '-server',
-            script: 'dist/nest/local/world/main.js',
-            watch: ['dist/nest/local/world', 'dist/nest/lib'],
+    apps: directories.map(dir => dir.split('/').pop())
+        .map(dir => ({
+            instances: (dir === 'world' ? 4 : 1),
+            name: (process.env.WORLD_CONSTANT || 'maiden') + '-' + (dir === 'world' ? 'server' : dir),
+            script: 'dist/nest/local/' + dir + '/main.js',
+            watch: ['dist/nest/local/' + dir, 'dist/nest/lib'],
             env: {
                 WORLD_CONSTANT: process.env.WORLD_CONSTANT,
                 WORLD_NAME: process.env.WORLD_NAME,
@@ -39,32 +15,5 @@ module.exports = {
             watch_options: {
                 'followSymlinks': false
             }
-        },
-        {
-            instances: 1,
-            name: (process.env.WORLD_CONSTANT || 'maiden') + '-map',
-            script: 'dist/nest/local/map/main.js',
-            watch: ['dist/nest/local/map', 'dist/nest/lib'],
-            env: {
-                WORLD_CONSTANT: process.env.WORLD_CONSTANT,
-                WORLD_NAME: process.env.WORLD_NAME
-            },
-            watch_options: {
-                'followSymlinks': false
-            }
-        },
-        {
-            instances: 1,
-            name: (process.env.WORLD_CONSTANT || 'maiden') + '-item',
-            script: 'dist/nest/local/item/main.js',
-            watch: ['dist/nest/local/item', 'dist/nest/lib'],
-            env: {
-                WORLD_CONSTANT: process.env.WORLD_CONSTANT,
-                WORLD_NAME: process.env.WORLD_NAME,
-            },
-            watch_options: {
-                'followSymlinks': false
-            }
-        }
-    ]
+        }))
 }
