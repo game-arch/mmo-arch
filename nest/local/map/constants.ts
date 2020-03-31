f8f440303a31d3306bb325b1aa12c7755f1c2706
Justin <jwhenry123@gmail.com>
2020-03-31 14:52:47 -0500

    add the instance id to the local instance number

--- nest/local/map/constants.ts
+++ nest/local/map/constants.ts
@@ -5,7 +5,7 @@ import { MAPS }                from '../../../shared/maps/config'
 export class MapConstants {
     static readonly MAP         = process.env.MAP
     static readonly CAPACITY    = 50
-    static readonly INSTANCE_ID = Number(process.env.MAP_INSTANCE_ID) || (Number(process.env.NODE_APP_INSTANCE) + 1)
+    static readonly INSTANCE_ID = Number(process.env.MAP_INSTANCE_ID || '0') + (Number(process.env.NODE_APP_INSTANCE || '0') + 1)
     static readonly MAPS        = MAPS
     static readonly NPC         = {
         tutorial    : TUTORIAL_NPC_DATA,
